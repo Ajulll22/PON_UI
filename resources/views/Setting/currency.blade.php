a54c8a21425f0d32443c9d48e52008517bc6653b
Ajulll22 <ajulrizki@gmail.com>
2023-04-19 11:32:25 +0700

    issue active delete

--- resources/views/Setting/currency.blade.php
+++ resources/views/Setting/currency.blade.php
@@ -308,13 +308,16 @@
                 {
                     searchable: false,
                     sortable: false,
+                    className: "text-center",
                     targets: -1,
                     data: null,
                     render: function(data) {
                         let follup =
                             `
-                                    <button id="edit-modal-show" style="text-decoration: none;" class="btn btn-outline-primary mg-r-5" type="button" title="Edit Item"><span class="icon ion-compose"></span></button>
-                                    <button id="delete-modal-show" style="text-decoration: none;" class="btn btn-outline-danger mg-r-5" type="button" title="Delete Item"><span class="icon ion-trash-a"></span></button>`
+                                    <button id="edit-modal-show" style="text-decoration: none;" class="btn btn-outline-primary mg-r-5" type="button" title="Edit Item"><span class="icon ion-compose"></span></button>`
+                        if (data.active != 1) {
+                            follup += `<button id="delete-modal-show" style="text-decoration: none;" class="btn btn-outline-danger mg-r-5" type="button" title="Delete Item"><span class="icon ion-trash-a"></span></button>`
+                        }
                         return follup;
                     }
                 }
