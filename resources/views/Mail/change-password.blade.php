255467a5c05e74d2fd5b291befce44cfbede8f13
Ajulll22 <ajulrizki@gmail.com>
2023-07-11 16:23:42 +0700

    issue

--- resources/views/Mail/change-password.blade.php
+++ resources/views/Mail/change-password.blade.php
@@ -3,7 +3,7 @@
 @section('email_content')
 <tr>
 	<td>
-		<p style="font-family: Helvetica; text-align: center; color: #053264; font-size: 20px; font-weight: bold;">Dear {{ $user_fullname }},</p>	
+		<p style="font-family: Helvetica; text-align: center; color: #053264; font-size: 20px; font-weight: bold;">Dear {{ $user_name }},</p>	
 		<p style="font-family: Helvetica; text-align: center; color: #053264; font-size: 16px; font-weight: bold;">Welcome to {{ config('constants.app_name') }}</p>
 	</td>
 </tr>									
